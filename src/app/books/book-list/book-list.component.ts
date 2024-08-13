1de7f3940693c9dde228f6dd23ed3fafc69b8183
Marcus Ross <marcus.ross@zahlenhelfer.de>
2024-08-13 15:37:02 +0200

    feat: Add book list component and update app component

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class BookListComponent {
 
+  books = [{
+    title: 'The Alchemist',
+    isbn: '978-0062315007',
+    price: 8.99,
+    rating: 4.8,
+  }, {
+    title: 'The Great Gatsby',
+    isbn: '978-0743273565',
+    price: 9.99,
+    rating: 4.5,
+  }
+  ];
+
 }
